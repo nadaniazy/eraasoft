4bba30651a7f9988ab7242b2c9ef5aa4fb9486e3
nadaniazy <101994378+nadaniazy@users.noreply.github.com>
2023-01-27 03:28:12 -0800

    first commit

---
+++ lib/screens/searchofproduct.dart
@@ -0,0 +1,61 @@
+import 'package:eraasoftproject/model/Products.dart';
+import 'package:eraasoftproject/network/cubit/cubit.dart';
+import 'package:eraasoftproject/network/cubit/states.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+class SearchOfProduct extends StatelessWidget {
+   SearchOfProduct() ;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context)=>appcubit()..p,
+      child: BlocConsumer<appcubit,appstates>(
+        listener: (context,appstates){},
+        builder: (context,states){
+          var cubit=appcubit.get(context);
+          return   cubit.p !=null? Scaffold(
+            backgroundColor: Colors.white,
+              body: Expanded(
+                child: ListView.builder(itemCount: cubit.p!.data!.productData!.length,itemBuilder: (context,index){
+                  return buildContainer(cubit.p!.data!.productData![index]);
+
+                })),
+
+          ):Center(child: CircularProgressIndicator());
+        },
+
+      ),
+    );
+  }
+
+  Container buildContainer(ProductData productData) {
+    return Container(
+      width: double.infinity,
+      height: 200,
+      child: Column(
+        children: [
+          Image(image: AssetImage("assets/chicken.png"),width: 100,height: 100,fit: BoxFit.contain,),
+          Text("${productData.name}",style: TextStyle(fontSize: 15),),
+          Text("4pcs,price",style: TextStyle(color: Colors.grey.shade300),),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Text("${productData.price}"),
+              Container(
+                width: 50,
+                height: 50,
+                child: Icon(Icons.add),
+                decoration: BoxDecoration(
+                  color: Colors.green,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
